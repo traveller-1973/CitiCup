fe386c5ad88be373fbb06621548655feb47a77d0
lewisiw <1907821839@qq.com>
2024-02-22 00:45:29 +0800

    添加了保存到excel中的代码，后续将根据问题加入异常处理代码

--- ClimbingTitle.py
+++ ClimbingTitle.py
@@ -3,11 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import  xlrd
+import xlrd
+import xlwt
 
 
 if __name__ == '__main__':
     # 打开Excel文件
+    # 在这里我们要用到的是先前爬取的所有A股股票数据的excel文件
     workbook = xlrd.open_workbook(r'E:\桌面\日常事务\大学学科竞赛\花旗杯\上市股票一览.xls')
     # 选择第一个工作表
     sheet = workbook.sheet_by_index(0)
@@ -16,6 +18,18 @@ if __name__ == '__main__':
     for row in range(1, sheet.nrows):
         first_column_data.append(sheet.cell_value(row, 0))
     result = [item.split('.')[0] for item in first_column_data]
+
+    # 创建新的Excel文件
+    new_workbook = xlwt.Workbook()
+    sheet = new_workbook.add_sheet('Sheet1')
+
+    # 写入标题行
+    sheet.write(0, 0, 'stock_code')
+    sheet.write(0, 1, 'post_title')
+    sheet.write(0, 2, 'publish_time')
+
+    row_index = 1  # 数据从第二行开始写入
+
     # 打印第一列数据
     for stock_code in result:
        # print(stock_code)
@@ -31,7 +45,10 @@ if __name__ == '__main__':
         chrome_options = Options()
         chrome_options.add_argument('--disable-popup-blocking')
         # driver = webdriver.Chrome(executable_path='/Users/yinliqi/Downloads/chromedriver_mac_arm64/chromedriver', options=chrome_options)
+
         driver = webdriver.Chrome('C:/Program Files/Google/Chrome/Application/chromedriver')
+       #  注意，这里需要下载chromedriver模拟浏览器的行为，并根据csdn进行相应的配置，对应的帖子的网址如下所示：
+       #  https://blog.csdn.net/zhoukeguai/article/details/113247342
 
         driver.get(url)
         driver.implicitly_wait(10)
@@ -53,9 +70,17 @@ if __name__ == '__main__':
                     for match_info in match_infos:
                         post_title = match_info[0]
                         post_publish_time = match_info[1]
+
+                        sheet.write(row_index, 0, stock_code)
+                        sheet.write(row_index, 1, post_title)
+                        sheet.write(row_index, 2, post_publish_time)
+
                         print("Post Title:", post_title)
                         print("Post Publish Time:", post_publish_time)
                         count += 1
+
+                        row_index += 1
+
                         if count == 3:  # 只打印前三个帖子信息
                             break
                 else:
@@ -65,4 +90,8 @@ if __name__ == '__main__':
         else:
             print("No <body inmaintabuse='1'> tag found.")
 
+        # 每次写入一条股票的信息后即时保存Excel文件
+        new_workbook.save('ClimbingTitle.xls')
 
+    # 最后再保存一次Excel文件
+    new_workbook.save('ClimbingTitle.xls')
